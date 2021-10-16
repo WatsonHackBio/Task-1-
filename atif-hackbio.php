8ae2bf257a36c9b06781d3e9a23359a59513606a
Sakshi Singh <92523465+WatsonHackBio@users.noreply.github.com>
2021-10-16 11:36:54 +0100

    Add files via upload

---
+++ atif-hackbio.php
@@ -0,0 +1,27 @@
+<?php
+
+    /*
+      Hack Bio Virtual Internship - Watson Team
+    */
+
+    // This is my name
+    echo "Muhammad Atif";
+
+    //this is break line element
+    echo "<br>";
+
+    // This is my email address
+    echo "itsatifiqbal@gmail.com";
+
+    //this is break line element
+    echo "<br>";
+
+    // This is my slack username 
+    echo "@atif";
+
+    //this is break line element
+    echo "<br>";
+
+    // This is my Biostack
+    echo "Genomics"
+?>
