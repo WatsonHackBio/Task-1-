fa189880369e2382e1c503c8c424c2ea886151fd
Team Watson <92523465+WatsonHackBio@users.noreply.github.com>
2021-10-19 19:08:06 +0200

    Add files via upload
    Name: Aribisala Jamiu Olaseni
    Slack username: @Aribisala
    biostack: Drug discovery
    Programme language: Python

---
+++ Aribisala script file.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print ("Name: Aribisala Jamiu Olaseni")
+
+
+# In[6]:
+
+
+print ("Email: jimexontop@gmail.com")
+
+
+# In[7]:
+
+
+print ("Biostack: Drug Discovery")
+
+
+# In[8]:
+
+
+print ("Slackusername @Aribisala")
+
+
+# In[9]:
+
+
+print ("Twitter handle: @jamiu_olaseni")
+
+
+# In[ ]:
+
+
+
+
